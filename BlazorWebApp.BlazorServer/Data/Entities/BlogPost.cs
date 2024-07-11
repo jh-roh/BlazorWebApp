344abc1ce631a4ac71bfdbcd6e97f272111b6960
노준호 <njh8810@naver.com>
2024-07-11 23:06:04 +0900

    Blazor Server 프로젝트 추가 연습용 커밋

---
+++ BlazorWebApp.BlazorServer/Data/Entities/BlogPost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebApp.BlazorServer.Data.Entities
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime? PublicshedOn { get; set; }
+    }
+
+}
---
+++ BlazorWebApp.BlazorServer/Data/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebApp.BlazorServer.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(25)]
+
+        public string FirstName { get;set;}
+
+        [MaxLength(25)]
+
+        public string? LastName { get; set;}
+
+        [Required, MaxLength(100)]
+        public string Email { get; set;}
+
+        [Required, MaxLength(30)]
+        public string Salt { get; set; }
+
+        [Required, MaxLength(125)]
+        public string Hash { get; set; }
+    }
+
+}
