090c20da858d5a81afd17628b3f7f1b33ea80b18
노준호 <njh8810@naver.com>
2024-07-14 18:20:56 +0900

    Blazor Server 로그인 관련 내용 커밋

---
+++ BlazorWebApp.BlazorServer/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebApp.BlazorServer.Models
+{
+    public class LoginModel
+    {
+        [Required, EmailAddress, DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required, MinLength(5)]
+        public string Password { get; set; }
+    }
+}
---
+++ BlazorWebApp.BlazorServer/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using BlazorWebApp.BlazorServer.Data;
+using BlazorWebApp.BlazorServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebApp.BlazorServer.Services
+{
+    public class UserService
+    {
+        private readonly BlogContext _context;
+
+        public UserService(BlogContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task LoginAsync(LoginModel model)
+        {
+            var dbUser = await _context.Users
+                                       .AsNoTracking()
+                                       .FirstOrDefaultAsync(u => u.Email == model.Username);
+
+            if(dbUser is not null)
+            {
+                //Login success
+            }
+            else
+            {
+                //Login failed
+            }
+                                       
+        }
+
+    }
+
+}
